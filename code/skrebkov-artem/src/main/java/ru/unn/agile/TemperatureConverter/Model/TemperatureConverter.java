b23122ca13617571070c80d7200047fedba40fdf
ArtemSkrebkov <skrebkov.art@gmail.com>
2016-11-08 18:47:15 +0300

    add empty project

---
+++ code/skrebkov-artem/src/main/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverter.java
@@ -0,0 +1,5 @@
+package ru.unn.agile.TemperatureConverter.Model;
+
+public class TemperatureConverter {
+
+}
---
+++ code/skrebkov-artem/src/test/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverterTest.java
@@ -0,0 +1,12 @@
+package ru.unn.agile.TemperatureConverter.Model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void failingTest() {
+        fail();
+    }
+}
