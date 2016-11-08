7ec4ad206416db093ccd3ad30ad4d3adc6900272
Darya Tropicheva <Darya Tropicheva>
2016-11-08 22:56:00 +0300

    Added implementation on lab1

---
+++ code/Tropicheva-Darya/src/main/java/ru/unn/agile/ComplexNumber/Model/CalculatorComplexNumber.java
@@ -0,0 +1,49 @@
+package ru.unn.agile.ComplexNumber.Model;
+
+/**
+ * Created by Дарья on 07.11.2016.
+ */
+public final class CalculatorComplexNumber {
+    private CalculatorComplexNumber() {
+    }
+
+    public static ComplexNumber add(final ComplexNumber complexA, final ComplexNumber complexB) {
+        return new ComplexNumber(complexA.getReal() + complexB.getReal(),
+                complexA.getImaginary() + complexB.getImaginary());
+    }
+
+    public static ComplexNumber sub(final ComplexNumber complexA, final ComplexNumber complexB) {
+        return new ComplexNumber(complexA.getReal() - complexB.getReal(),
+                complexA.getImaginary() - complexB.getImaginary());
+    }
+
+    public static ComplexNumber mult(final ComplexNumber complexA, final ComplexNumber complexB) {
+        float real = complexA.getReal() * complexB.getReal()
+                - complexA.getImaginary() * complexB.getImaginary();
+        float im = complexA.getReal() * complexB.getImaginary()
+                + complexA.getImaginary() * complexB.getReal();
+        return new ComplexNumber(real, im);
+    }
+
+
+    public static ComplexNumber div(final ComplexNumber complexA, final ComplexNumber complexB) {
+        if ((Float.compare(complexB.getReal(), 0.f) == 0)
+                && (Float.compare(complexB.getImaginary(), 0.f)) == 0) {
+            throw new ArithmeticException("Argument 'divisor' is 0");
+        } else {
+            float factor = complexB.getReal() * complexB.getReal()
+                    + complexB.getImaginary() * complexB.getImaginary();
+            ComplexNumber representComplexB = new ComplexNumber((complexB.getReal() / factor),
+                    -(complexB.getImaginary() / factor));
+            return mult(complexA, representComplexB);
+        }
+    }
+
+    public static float abs(final ComplexNumber complexNumberA) {
+        return (float) Math.hypot(complexNumberA.getReal(), complexNumberA.getImaginary());
+    }
+
+    public static double getArgument(final ComplexNumber complexNumber) {
+        return Math.atan2((double) complexNumber.getImaginary(), (double) complexNumber.getReal());
+    }
+}
---
+++ code/Tropicheva-Darya/src/main/java/ru/unn/agile/ComplexNumberCalculator/ComplexNumberCalculator.java
@@ -0,0 +1,7 @@
+package ru.unn.agile.ComplexNumberCalculator;
+
+/**
+ * Created by Дарья on 06.11.2016.
+ */
+public class ComplexNumberCalculator {
+}
