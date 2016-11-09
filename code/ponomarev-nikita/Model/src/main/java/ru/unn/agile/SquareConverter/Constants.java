6047a3010613f5b407b53059db51f66493f87ff3
Nikita Ponomarev <ponom.nikita@gmail.com>
2016-11-09 22:24:53 +0300

    Fixed code according to code review comments

---
+++ code/ponomarev-nikita/Model/src/main/java/ru/unn/agile/SquareConverter/Constants.java
@@ -0,0 +1,17 @@
+package ru.unn.agile.SquareConverter;
+
+/**
+ * Created by ponom on 09.11.2016.
+ */
+public class Constants {
+    public static final double TO_KILOMETER_MULTIPLIER = 0.000001;
+    public static final double TO_HECTARE_MULTIPLIER = 0.0001;
+    public static final double TO_AR_MULTIPLIER = 0.01;
+    public static final double TO_SQR_SANTIMETER_MULTIPLIER = 10000;
+    public static final double TO_SQR_MILLIMETER_MULTIPLIER = 1000000;
+    public static final double TO_SQR_YARD_MULTIPLIER = 1.196;
+    public static final double TO_SQR_FOOT_MULTIPLIER = 10.76;
+    public static final double TO_SQR_INCH_MULTIPLIER = 1550;
+    public static final double TO_SQR_ACRE_MULTIPLIER = 0.0002471;
+    public static final double TO_SQR_MILE_MULTIPLIER = 0.0000003861;
+}
