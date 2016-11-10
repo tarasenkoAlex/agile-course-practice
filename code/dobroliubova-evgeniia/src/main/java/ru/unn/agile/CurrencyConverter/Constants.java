f4c8d6d23c7cf5d9b5558c0df731df9ff074c5b5
DobroliubovaEvgeniia <Jane8393@mail.ru>
2016-11-10 10:57:13 +0300

    Added class Constants

---
+++ code/dobroliubova-evgeniia/src/main/java/ru/unn/agile/CurrencyConverter/Constants.java
@@ -0,0 +1,15 @@
+package ru.unn.agile.CurrencyConverter;
+
+/**
+ * Created by evdo0116 on 10.11.2016.
+ */
+public final class Constants {
+    static final double RUBLE = 1;
+    static final double DOLLAR = 62.6200;
+    static final double EURO = 69.1254;
+    static final double HRYVNA = 24.3255;
+    static final double YEN = 60.4592;
+    static final double FRANC = 63.5256;
+
+    private Constants() { }
+}
--- code/dobroliubova-evgeniia/src/main/java/ru/unn/agile/CurrencyConverter/Converter.java
+++ code/dobroliubova-evgeniia/src/main/java/ru/unn/agile/CurrencyConverter/Converter.java
@@ -5,13 +5,6 @@ package ru.unn.agile.CurrencyConverter;
  * Created by Jane on 03.11.2016.
  */
 public class Converter {
-    static final double RUBLE = 1;
-    static final double DOLLAR = 62.6200;
-    static final double EURO = 69.1254;
-    static final double HRYVNA = 24.3255;
-    static final double YEN = 60.4592;
-    static final double FRANC = 63.5256;
-
     public double execute(final double amount, final double fromCurrency, final double toCurrency) {
         if (amount < 0) {
             throw new IllegalArgumentException("Amount must be positive number");
