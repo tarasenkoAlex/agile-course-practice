facbb3db71f3c7201ec0fde1401b0b9c768492e2
Valentine Rybin <valentine.rybin@gmail.com>
2016-11-10 23:49:49 +0300

    Added Polynomial constructors

--- code/rybin-valentine/src/main/java/com/github/rybval/Polynomial/Model/Polynomial.java
+++ code/rybin-valentine/src/main/java/com/github/rybval/Polynomial/Model/Polynomial.java
@@ -1,16 +1,33 @@
 package com.github.rybval.Polynomial.Model;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
 import java.util.Arrays;
 
 public class Polynomial {
-    private final ArrayList monomials;
+    private final Map<Integer, Monomial> monomials;
 
     Polynomial() {
-        monomials = new ArrayList<Monomial>();
+        monomials = new HashMap<Integer, Monomial>();
+    }
+
+    Polynomial(final List<Monomial> monomialsList) {
+        monomials = new HashMap<Integer, Monomial>();
+        for (Monomial monomialIn : monomialsList) {
+            int power = monomialIn.getPower();
+            Monomial monomialToPut;
+            if (monomials.containsKey(power)) {
+                monomialToPut = monomials.get(power).add(monomialIn);
+            } else {
+                monomialToPut = monomialIn;
+            }
+            monomials.put(power, monomialToPut);
+        }
     }
 
     Polynomial(final Monomial[] monomials) {
-        this.monomials = new ArrayList<Monomial>(Arrays.asList(monomials));
+        this(Arrays.asList(monomials));
     }
+
 }
--- code/rybin-valentine/src/test/java/com/github/rybval/Polynomial/Model/PolynomialTest.java
+++ code/rybin-valentine/src/test/java/com/github/rybval/Polynomial/Model/PolynomialTest.java
@@ -1,12 +1,25 @@
 package com.github.rybval.Polynomial.Model;
 
 import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class PolynomialTest {
     @Test
     public void canCreatePolynomial() {
         Polynomial polynomial = new Polynomial();
+        assertNotNull(polynomial);
+    }
+
+    @Test
+    public void canCreatePolynomialWithListOfMonomials() {
+        List<Monomial> monomialsList = new ArrayList<Monomial>();
+        monomialsList.add(new Monomial(5, 5.4));
+        monomialsList.add(new Monomial(3, 2.1));
+
+        Polynomial polynomial = new Polynomial(monomialsList);
+
         assertNotNull(polynomial);
     }
 
