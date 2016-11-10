bee112e4c2df1fad9cc874e754f2ad7927c41208
Павел <inbox40@inbox.ru>
2016-11-10 03:37:19 +0300

    Fixing comments by Pavel Kochetov

--- code/kochetov-pavel/src/main/java/ru/unn/agile/Credit/Model/Credit.java
+++ code/kochetov-pavel/src/main/java/ru/unn/agile/Credit/Model/Credit.java
@@ -6,23 +6,23 @@ public class Credit {
     private final double sum;
     private final double months;
     private final double percent;
-    private final double percent_max_value = 100;
-    private final double months_in_the_year = 12;
+    private final double percentMaxValue = 10 * 10;
+    private final double monthsInTheYear = 12;
 
     Credit(final double sum, final double months, final double percent) {
-        if(sum < 0) {
+        if (sum < 0) {
             throw new InvalidParameterException("Sum must be positive!");
         }
-        if(months < 0) {
+        if (months < 0) {
             throw new InvalidParameterException("Months must be positive!");
         }
-        if((percent < 0) || (percent > 100)) {
+        if ((percent < 0) || (percent > 100)) {
             throw new InvalidParameterException("Percents must be from 0 to 100!");
         }
-        if(months == 0) {
+        if (months == 0) {
             throw new InvalidParameterException("Months must be not null!");
         }
-        if((sum == 0) && (months == 0) && (percent == 0)) {
+        if ((sum == 0) && (months == 0) && (percent == 0)) {
             throw new InvalidParameterException("Parameters are wrong!");
         }
         this.sum = sum;
@@ -31,9 +31,9 @@ public class Credit {
     }
 
     public double countPayment() {
-        double normalized_percent = (percent / percent_max_value) / months_in_the_year;
-        double fraction_count_pay = normalized_percent / (Math.pow(1 + normalized_percent, months) - 1);
-        return sum * (normalized_percent + fraction_count_pay);
+        double normalizedPercent = (percent / percentMaxValue) / monthsInTheYear;
+        double fraction_count_pay = normalizedPercent / (Math.pow(1 + normalizedPercent, months) - 1);
+        return sum * (normalizedPercent + fraction_count_pay);
     }
 
     public double countTotalSum() {
