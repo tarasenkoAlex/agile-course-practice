d679d6aff03a6bb936ba7f222c91b32e98b86648
Nikita-K <n.yu.kudriavtsev@gmail.com>
2016-11-11 00:35:39 +0300

    Refactored parameterized tests

--- code/kudriavtsev-nikita/src/test/java/ru/unn/agile/Triangle/Model/WhenCallMethodGetIncircleOrCircumscribedCircle.java
+++ code/kudriavtsev-nikita/src/test/java/ru/unn/agile/Triangle/Model/WhenCallMethodGetIncircleOrCircumscribedCircle.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
-public class WhenCallMethodGetIncircleCircle {
+public class WhenCallMethodGetIncircleOrCircumscribedCircle {
     private final double delta = 0.001;
 
     @Parameterized.Parameters
@@ -28,22 +28,25 @@ public class WhenCallMethodGetIncircleCircle {
                                                    new Point2D(0, 0));
 
         return Arrays.asList(new Object[][] {
-                { new Point2D(0.29289321, 0.29289321), 0.29289321, simpleTriangle },
-                { new Point2D(0.57911321, -0.72936959), 0.94469579, difficultTriangle },
-                { new Point2D(2, 0), 0, degeneracyTriangle }
+                { new Point2D(0.5, 0.5), 0.70710678, simpleTriangle.getCircCircle()},
+                { new Point2D(0.44259259, -1.7212963), 2.32128817, difficultTriangle.getCircCircle() },
+                { new Point2D(Double.NaN, Double.NaN), Double.NaN, degeneracyTriangle.getCircCircle() },
+                { new Point2D(0.29289321, 0.29289321), 0.29289321, simpleTriangle.getIncircleCircle() },
+                { new Point2D(0.57911321, -0.72936959), 0.94469579, difficultTriangle.getIncircleCircle() },
+                { new Point2D(2, 0), 0, degeneracyTriangle.getIncircleCircle() }
         });
     }
 
     private Point2D expectCenter;
     private double expectRadius;
-    private Triangle triangle;
+    private Circle actualCircle;
 
-    public WhenCallMethodGetIncircleCircle(final Point2D expectCenter,
-                                           final double expectRadius,
-                                           final Triangle triangle) {
+    public WhenCallMethodGetIncircleOrCircumscribedCircle(final Point2D expectCenter,
+                                                          final double expectRadius,
+                                                          final Circle actualCircle) {
         this.expectCenter = expectCenter;
         this.expectRadius = expectRadius;
-        this.triangle = triangle;
+        this.actualCircle = actualCircle;
     }
 
     @Test
@@ -51,7 +54,6 @@ public class WhenCallMethodGetIncircleCircle {
         double expectX = expectCenter.getX();
         double expectY = expectCenter.getY();
 
-        Circle actualCircle = triangle.getIncircleCircle();
         Point2D actualCenter = actualCircle.getCenter();
 
         assertEquals(expectX, actualCenter.getX(), delta);
