5f615a8cd2bd62dae667760e8dfd71d84f5b2f19
Ruslan Israfilov <ruslan.is@outlook.com>
2016-11-12 20:12:33 +0300

    Add implementation of Task constructor and getters

---
+++ code/israfilov-ruslan/src/main/java/ru/unn/agile/todoapp/model/Task.java
@@ -0,0 +1,29 @@
+package ru.unn.agile.todoapp.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Task {
+    private final String description;
+    private final Date expirationDate;
+
+    public Task(final String description, final Date expirationDate)
+            throws NullPointerException, IllegalArgumentException {
+        Objects.requireNonNull(description, "description is not able to be null");
+        Objects.requireNonNull(expirationDate, "expirationDate is not able to be null");
+        if (description.trim().length() == 0) {
+            String message = "description is not able to be whitespaces string";
+            throw new IllegalArgumentException(message);
+        }
+        this.description = description;
+        this.expirationDate = expirationDate;
+    }
+
+    public final String getDescription() {
+        return description;
+    }
+
+    public final Date getExpirationDate() {
+        return expirationDate;
+    }
+}
