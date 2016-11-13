3187893eb24fa1c1a8d67b81351b2ff4256d91ee
zaydullova-alina <zaydullova.alina@icloud.com>
2016-11-13 19:00:03 +0400

    Fix testTreeCreate() test

--- code/zaydullova-alina/src/main/java/ru/unn/agile/TreeSort/Model/Tree.java
+++ code/zaydullova-alina/src/main/java/ru/unn/agile/TreeSort/Model/Tree.java
@@ -4,14 +4,25 @@ package ru.unn.agile.TreeSort.Model;
  * Created by Pavel on 13.11.16.
  */
 public class Tree {
+    private Integer keyValue;
+
     public Tree(Integer key) {
+        keyValue = key;
     }
 
     public Integer getKey() {
-        return null;
+        return keyValue;
     }
 
     public Tree getLeft() {
         return null;
+    }
+
+    public Tree getRight() {
+        return null;
+    }
+
+    public void insert(Integer key) {
+
     }
 }
