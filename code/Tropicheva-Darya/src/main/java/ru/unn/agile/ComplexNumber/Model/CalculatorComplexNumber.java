7927b9cf7e6a9c75ff24e70e134e9fae6cad3a54
Sirotin Nikita <niksir20@mail.ru>
2016-11-20 23:21:13 +0300

    Update CalculatorComplexNumber.java

--- code/Tropicheva-Darya/src/main/java/ru/unn/agile/ComplexNumber/Model/CalculatorComplexNumber.java
+++ code/Tropicheva-Darya/src/main/java/ru/unn/agile/ComplexNumber/Model/CalculatorComplexNumber.java
@@ -7,60 +7,60 @@ public final class CalculatorComplexNumber {
     private CalculatorComplexNumber() {
     }
 
-    public static ComplexNumber add(final ComplexNumber complexA, final ComplexNumber complexB) {
+    public static Complex add(final Complex complexA, final Complex complexB) {
         checkNonNullNumber(complexA);
         checkNonNullNumber(complexB);
-        return new ComplexNumber(complexA.getReal() + complexB.getReal(),
+        return new Complex(complexA.getReal() + complexB.getReal(),
                 complexA.getImaginary() + complexB.getImaginary());
     }
 
-    public static ComplexNumber sub(final ComplexNumber complexA, final ComplexNumber complexB) {
+    public static Complex sub(final Complex complexA, final Complex complexB) {
         checkNonNullNumber(complexA);
         checkNonNullNumber(complexB);
-        return new ComplexNumber(complexA.getReal() - complexB.getReal(),
+        return new Complex(complexA.getReal() - complexB.getReal(),
                 complexA.getImaginary() - complexB.getImaginary());
     }
 
-    public static ComplexNumber mult(final ComplexNumber complexA, final ComplexNumber complexB) {
+    public static Complex mult(final Complex complexA, final Complex complexB) {
         checkNonNullNumber(complexA);
         checkNonNullNumber(complexB);
         float real = complexA.getReal() * complexB.getReal()
                 - complexA.getImaginary() * complexB.getImaginary();
         float im = complexA.getReal() * complexB.getImaginary()
                 + complexA.getImaginary() * complexB.getReal();
-        return new ComplexNumber(real, im);
+        return new Complex(real, im);
     }
 
-    private static void checkNonZeroNumber(final ComplexNumber complexNumber) {
+    private static void checkNonZeroNumber(final Complex complexNumber) {
         if (Float.compare(complexNumber.getReal(), 0.f) == 0
                 && Float.compare(complexNumber.getImaginary(), 0.f) == 0) {
             throw new ArithmeticException("Divider can't be zero");
         }
     }
 
-    private static void checkNonNullNumber(final ComplexNumber complexNumber) {
+    private static void checkNonNullNumber(final Complex complexNumber) {
         if (complexNumber == null) {
             throw new IllegalArgumentException("Parameter can't be null");
         }
     }
 
-    public static ComplexNumber div(final ComplexNumber complexA, final ComplexNumber complexB) {
+    public static Complex div(final Complex complexA, final Complex complexB) {
         checkNonNullNumber(complexA);
         checkNonNullNumber(complexB);
         checkNonZeroNumber(complexB);
         float factor = complexB.getReal() * complexB.getReal()
                 + complexB.getImaginary() * complexB.getImaginary();
-        ComplexNumber representComplexB = new ComplexNumber(complexB.getReal() / factor,
+        Complex representComplexB = new Complex(complexB.getReal() / factor,
                 -complexB.getImaginary() / factor);
         return mult(complexA, representComplexB);
     }
 
-    public static float abs(final ComplexNumber complexNumber) {
+    public static float abs(final Complex complexNumber) {
         checkNonNullNumber(complexNumber);
         return (float) Math.hypot(complexNumber.getReal(), complexNumber.getImaginary());
     }
 
-    public static double getArgument(final ComplexNumber complexNumber) {
+    public static double getArgument(final Complex complexNumber) {
         checkNonNullNumber(complexNumber);
         return Math.atan2((double) complexNumber.getImaginary(), (double) complexNumber.getReal());
     }
