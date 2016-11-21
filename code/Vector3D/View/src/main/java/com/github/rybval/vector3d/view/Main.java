e99a187b37009cec778689d22b46146bf4f7cdc1
Valentine Rybin <valentine.rybin@gmail.com>
2016-11-22 00:21:18 +0300

    Made project structure for View and ViewModel

---
+++ code/Vector3D/View/src/main/java/com/github/rybval/vector3d/view/Main.java
@@ -0,0 +1,22 @@
+package com.github.rybval.vector3d.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Vector3D.fxml"));
+        primaryStage.setTitle("3D Vector Operations");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
