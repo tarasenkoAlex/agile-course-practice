8ef2eb14192946e35ff0563bb023d37648e6f959
Audice <karchkov.denis@mail.ru>
2016-11-22 10:59:51 +0300

    Modified test class

--- code/Karchkov-Denis/src/test/java/com/github/audice/matrixoperation/model/CalculateMultyAndPlussMatrixTest.java
+++ code/Karchkov-Denis/src/test/java/com/github/audice/matrixoperation/model/CalculateMultyAndPlussMatrixTest.java
@@ -1,6 +1,5 @@
 package com.github.audice.matrixoperation.model;
 
-import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -20,54 +19,64 @@ public class CalculateMultyAndPlussMatrixTest {
         }
         return arrayText;
     }
+
     @Test
     public void canCreateMutrixMultyAndAddWithInitialValues() {
         matrix = new Matrix(rows, columns, testArray);
         assertNotNull(matrix);
     }
+
     @Test
     public void cantCreateMutrix() {
         assertNull(matrix);
     }
+
     @Test
     public void compareMatrixAndTestArrayInTextImage() {
         matrix = new Matrix(rows, columns, new float[]{1, 2, 3, 4, 5, 6});
-        Assert.assertEquals(arryeToString(new float[]{1, 2, 3, 4, 5, 6}), matrix.toString());
+        assertEquals(arryeToString(new float[]{1, 2, 3, 4, 5, 6}), matrix.toString());
     }
+
     @Test
     public void compareMatrixAndTestArrayInTextImageOnNotEquals() {
         matrix = new Matrix(rows, columns, new float[]{1, 2, 3, 4, 5, 7});
-        Assert.assertNotEquals(arryeToString(new float[]{1, 2, 3, 4, 5, 6}), matrix.toString());
+        assertNotEquals(arryeToString(new float[]{1, 2, 3, 4, 5, 6}), matrix.toString());
     }
+
     @Test
     public void arrayInTextImageOnNotEmpty() {
         matrix = new Matrix(rows, columns, new float[]{1, 2, 3, 4, 5, 7});
         assertFalse(matrix.toString().isEmpty());
     }
+
     @Test
     public void arrayInTextImageOnNull() {
         matrix = new Matrix(0, 0, null);
         assertNull(matrix.toString());
     }
+
     @Test
     public void canGetNumberOfRows() {
         matrix = new Matrix(5, columns, testArray);
-        Assert.assertEquals(5, matrix.getNumR());
+        assertEquals(5, matrix.getNumR());
     }
+
     @Test
     public void canGetNumberOfColumns() {
         matrix = new Matrix(rows, 7, testArray);
-        Assert.assertEquals(7, matrix.getNumC());
+        assertEquals(7, matrix.getNumC());
     }
+
     @Test
     public void getElementOfIndex() {
         matrix = new Matrix(rows, columns, testArray);
-        Assert.assertEquals(testArray[3], matrix.getElementMatrix(3), delta);
+        assertEquals(testArray[3], matrix.getElementMatrix(3), delta);
     }
+
     @Test
     public void getIsNotElementOfIndex() {
         matrix = new Matrix(rows, columns, testArray);
-        Assert.assertNotEquals(100f, matrix.getElementMatrix(3), delta);
+        assertNotEquals(100f, matrix.getElementMatrix(3), delta);
     }
 
     @Test
@@ -75,29 +84,33 @@ public class CalculateMultyAndPlussMatrixTest {
         first = new Matrix(2, 2, new float[]{1, 1, 1, 1});
         second = new Matrix(2, 2, new float[]{2, 2, 2, 2});
         String testM = "3.0,3.0,3.0,3.0,";
-        Assert.assertEquals(testM, Matrix.sum(first, second).toString());
+        assertEquals(testM, Matrix.sum(first, second).toString());
     }
+
     @Test
     public void canSumTwoMatrixWithDifferentNumberOfRows() {
         first = new Matrix(3, 2, testArray);
         second = new Matrix(2, 2, new float[]{2, 2, 2, 2});
         String testM = "3.0,3.0,3.0,3.0,";
-        Assert.assertEquals(null, Matrix.sum(first, second));
+        assertEquals(null, Matrix.sum(first, second));
     }
+
     @Test
     public void canSumTwoMatrixWithDifferentNumberOfColumnsAndRows() {
         first = new Matrix(3, 3, new float[]{1, 1, 1, 1, 2, 2, 1, 1, 2});
         second = new Matrix(2, 2, new float[]{2, 2, 2, 2});
         String testM = "3.0,3.0,3.0,3.0,";
-        Assert.assertEquals(null, Matrix.sum(first, second));
+        assertEquals(null, Matrix.sum(first, second));
     }
+
     @Test
     public void canMultyTwoMatrix() {
         first = new Matrix(2, 2, new float[]{1, 3, 4, 2});
         second = new Matrix(2, 2, new float[]{1, 2, 1, 3});
         String result = "4.0,11.0,6.0,14.0,";
-        Assert.assertEquals(result, Matrix.multiply(first, second).toString());
+        assertEquals(result, Matrix.multiply(first, second).toString());
     }
+
     @Test
     public void canMultyTwoMatrixWithDifferentNumbersOfColumnsAndRows() {
         first = new Matrix(2, 3, new float[]{1, 3, 4, 2, 3, 4});
