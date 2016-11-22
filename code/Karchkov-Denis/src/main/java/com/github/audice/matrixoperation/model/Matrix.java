75cf1dd34e340265362e131567067851f3e4aefd
Audice <karchkov.denis@mail.ru>
2016-11-22 11:11:18 +0300

    Entered small change

--- code/Karchkov-Denis/src/main/java/com/github/audice/matrixoperation/model/Matrix.java
+++ code/Karchkov-Denis/src/main/java/com/github/audice/matrixoperation/model/Matrix.java
@@ -14,6 +14,7 @@ public class Matrix {
             this.elementsOfMatrix = Arrays.copyOf(matrixElements, matrixElements.length);
         }
     }
+
     public int getNumR() {
         return this.rows;
     }
