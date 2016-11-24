e3788bb54844572b3480d9b24fd2bef5fc27903c
Valentine Rybin <valentine.rybin@gmail.com>
2016-11-24 15:57:43 +0300

    Added fields and few methods of ViewModel
    Created fields to represent controls of forms,
    their accessor methods,
    and parse methods.

--- code/Vector3D/ViewModel/src/main/java/com/github/rybval/vector3d/viewmodel/Vector3DViewModel.java
+++ code/Vector3D/ViewModel/src/main/java/com/github/rybval/vector3d/viewmodel/Vector3DViewModel.java
@@ -1,4 +1,130 @@
 package com.github.rybval.vector3d.viewmodel;
 
+import java.util.regex.Pattern;
+import ru.unn.agile.vector3d.model.Vector3D;
+
 public class Vector3DViewModel {
+    private OperationTab activeTab;
+    private String vectorText = "";
+    private String dotProductOperandText = "";
+    private String crossProductOperandText = "";
+    private String normResultText = "";
+    private String normalizationResultText = "";
+    private String dotProductResultText = "";
+    private String crossProductResultText = "";
+    private Vector3D vector;
+    private Vector3D dotProductOperand;
+    private Vector3D crossProductOperand;
+    private boolean buttonEnabled = false;
+
+    public Vector3DViewModel() {
+        activeTab = OperationTab.NORM;
+    }
+
+    public OperationTab getActiveTab() {
+        return activeTab;
+    }
+
+    public void setActiveTab(final OperationTab newActiveTab) {
+        activeTab = newActiveTab;
+    }
+
+    public boolean isButtonEnabled() {
+        return buttonEnabled;
+    }
+
+    public void enableButton() {
+        buttonEnabled = true;
+    }
+
+    public void disableButton() {
+        buttonEnabled = false;
+    }
+
+    public String getVectorText() {
+        return vectorText;
+    }
+
+    public void setVectorText(final String text) {
+        vectorText = text;
+    }
+
+    public String getDotProductOperandText() {
+        return dotProductOperandText;
+    }
+
+    public void setDotProductOperandText(final String text) {
+        dotProductOperandText = text;
+    }
+
+    public String getCrossProductOperandText() {
+        return crossProductOperandText;
+    }
+
+    public void setCrossProductOperandText(final String text) {
+        crossProductOperandText = text;
+    }
+
+    public String getNormResultText() {
+        return normResultText;
+    }
+
+    public void setNormResultText(final String text) {
+        normResultText = text;
+    }
+
+    public String getNormalizationResultText() {
+        return normalizationResultText;
+    }
+
+    public void setNormalizationResultText(final String text) {
+        normalizationResultText = text;
+    }
+
+    public String getDotProductResultText() {
+        return dotProductResultText;
+    }
+
+    public void setDotProductResultText(final String text) {
+        dotProductResultText = text;
+    }
+
+    public String getCrossProductResultText() {
+        return crossProductResultText;
+    }
+
+    public void setCrossProductResultText(final String text) {
+        crossProductResultText = text;
+    }
+
+    public boolean validate(final String text) {
+        String coordPattern = "[\\+-]?[0-9]+(\\.[0-9]+)?";
+        String vectorPattern = " *" + coordPattern + ", *"
+                                    + coordPattern + ", *"
+                                    + coordPattern + " *";
+        String vectorInBracketsPattern = " *\\(" + vectorPattern + "\\) *";
+        return Pattern.matches("^" + vectorPattern + "$", text)
+            || Pattern.matches("^" + vectorInBracketsPattern + "$", text);
+    }
+
+    public Vector3D vectorFromString(final String text) {
+        if (!validate(text)) {
+            throw new IllegalArgumentException();
+        }
+        String[] coordTexts = text.replaceAll("[ \\+\\(\\)]+", "").split(",");
+        double[] coords = new double[coordTexts.length];
+        for (int i = 0; i < coordTexts.length; i++) {
+            coords[i] = Double.parseDouble(coordTexts[i]);
+        }
+        return new Vector3D(coords);
+    }
+
+
+}
+
+enum OperationTab {
+    NORM,
+    NORMALIZATION,
+    DOTPRODUCT,
+    CROSSPRODUCT
 }
--- code/Vector3D/ViewModel/src/test/java/com/github/rybval/vector3d/viewmodel/Vector3DViewModelTest.java
+++ code/Vector3D/ViewModel/src/test/java/com/github/rybval/vector3d/viewmodel/Vector3DViewModelTest.java
@@ -1,13 +1,191 @@
 package com.github.rybval.vector3d.viewmodel;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import ru.unn.agile.vector3d.model.Vector3D;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class Vector3DViewModelTest {
-    @Ignore @Test
-    public void fakeTest() {
-        fail("Tests will be done");
+    private Vector3DViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new Vector3DViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void isNormTabActiveOnStart() {
+        assertEquals(OperationTab.NORM, viewModel.getActiveTab());
+    }
+
+    @Test
+    public void canGetButtonStatus() {
+        boolean enabled = viewModel.isButtonEnabled();
+        assertNotNull(enabled);
+    }
+
+    @Test
+    public void isButtonDisabledOnStart() {
+        Vector3DViewModel viewModel = new Vector3DViewModel();
+        assertEquals(false, viewModel.isButtonEnabled());
+    }
+
+    @Test
+    public void canEnableButton() {
+        viewModel.enableButton();
+        assertEquals(true, viewModel.isButtonEnabled());
+    }
+
+    @Test
+    public void canDisableButton() {
+        viewModel.enableButton();
+        viewModel.disableButton();
+        assertEquals(false, viewModel.isButtonEnabled());
+    }
+
+    @Test
+    public void canGetVectorText() {
+        String text = viewModel.getVectorText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetVectorText() {
+        String text = "some text";
+        viewModel.setVectorText(text);
+        assertEquals(text, viewModel.getVectorText());
+    }
+
+    @Test
+    public void canGetDotProductOperandText() {
+        String text = viewModel.getDotProductOperandText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetDotProductOperandText() {
+        String text = "some text";
+        viewModel.setDotProductOperandText(text);
+        assertEquals(text, viewModel.getDotProductOperandText());
+    }
+
+    @Test
+    public void canGetCrossProductOperandText() {
+        String text = viewModel.getCrossProductOperandText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetCrossProductOperandText() {
+        String text = "some text";
+        viewModel.setCrossProductOperandText(text);
+        assertEquals(text, viewModel.getCrossProductOperandText());
+    }
+
+    @Test
+    public void canGetNormResultText() {
+        String text = viewModel.getNormResultText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetNormResultText() {
+        String text = "some text";
+        viewModel.setNormResultText(text);
+        assertEquals(text, viewModel.getNormResultText());
+    }
+
+    @Test
+    public void canGetNormalizationResultText() {
+        String text = viewModel.getNormalizationResultText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetNormalizationResultText() {
+        String text = "some text";
+        viewModel.setNormalizationResultText(text);
+        assertEquals(text, viewModel.getNormalizationResultText());
+    }
+
+    @Test
+    public void canGetDotProductResultText() {
+        String text = viewModel.getDotProductResultText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetDotProductResultText() {
+        String text = "some text";
+        viewModel.setDotProductResultText(text);
+        assertEquals(text, viewModel.getDotProductResultText());
+    }
+
+    @Test
+    public void canGetCrossProductResultText() {
+        String text = viewModel.getCrossProductResultText();
+        assertNotNull(text);
+    }
+
+    @Test
+    public void canSetCrossProductResultText() {
+        String text = "some text";
+        viewModel.setCrossProductResultText(text);
+        assertEquals(text, viewModel.getCrossProductResultText());
+    }
+
+    @Test
+    public void areAllTextsEmptyOnStart() {
+        assertEquals("", viewModel.getVectorText());
+        assertEquals("", viewModel.getDotProductOperandText());
+        assertEquals("", viewModel.getCrossProductOperandText());
+        assertEquals("", viewModel.getNormResultText());
+        assertEquals("", viewModel.getNormalizationResultText());
+        assertEquals("", viewModel.getDotProductResultText());
+        assertEquals("", viewModel.getCrossProductResultText());
+    }
+
+    @Test
+    public void isValidationCorrectForValidText() {
+        String text = "5.6, 8, 9.000";
+        assertTrue(viewModel.validate(text));
+    }
+
+    @Test
+    public void isValidationCorrectForInvalidText() {
+        String text = "asdfv";
+        assertFalse(viewModel.validate(text));
+    }
+
+    @Test
+    public void isValidationCorrectForValidTextInParentheses() {
+        String text = "(5.6, 8, 9.000)";
+        assertTrue(viewModel.validate(text));
+    }
+
+    @Test
+    public void isValidationCorrectForValidTextWithOneBracket() {
+        String text = "(5.6, 8, 9.000";
+        assertFalse(viewModel.validate(text));
+    }
+
+    @Test
+    public void canConvertValidStringToVector() {
+        String text = "(5.6, 8, 9.000)";
+        Vector3D vector = viewModel.vectorFromString(text);
+        assertEquals(new Vector3D(5.6, 8, 9), vector);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cantConvertInvalidStringToVector() {
+        String text = "(5.6, 8, 9.000";
+        Vector3D vector = viewModel.vectorFromString(text);
     }
 }
