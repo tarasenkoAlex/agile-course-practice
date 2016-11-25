21a0baeb4d22cfe32dec24477073ce7532baa58d
Valentine Rybin <valentine.rybin@gmail.com>
2016-11-26 00:27:43 +0300

    Defined fields for UI controls in Controller.
    Also fixed naming of controls.

--- code/Vector3D/View/src/main/java/ru/unn/agile/vector3d/view/Controller.java
+++ code/Vector3D/View/src/main/java/ru/unn/agile/vector3d/view/Controller.java
@@ -1,4 +1,37 @@
 package ru.unn.agile.vector3d.view;
 
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.fxml.FXML;
+import ru.unn.agile.vector3d.viewmodel.ViewModel;
+
 public class Controller {
+    @FXML
+    private TabPane opsTabPane;
+    @FXML
+    private Tab normTab;
+    @FXML
+    private Tab normalizationTab;
+    @FXML
+    private Tab dotProductTab;
+    @FXML
+    private Tab crossProductTab;
+    @FXML
+    private TextField vectorTextField;
+    @FXML
+    private TextField normResultTextField;
+    @FXML
+    private TextField normalizationResultTextField;
+    @FXML
+    private TextField dotProductOperandTextField;
+    @FXML
+    private TextField dotProductResultTextField;
+    @FXML
+    private TextField crossProductOperandTextField;
+    @FXML
+    private TextField crossProductResultTextField;
+    @FXML
+    private Button calculateButton;
 }
