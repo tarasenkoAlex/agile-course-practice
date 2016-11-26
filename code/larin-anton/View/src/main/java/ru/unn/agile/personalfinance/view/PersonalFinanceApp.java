d3c078c21113367c59c1fd1a6eb8d499117e6c42
Ruslan Israfilov <ruslan.is@outlook.com>
2016-11-26 12:24:19 +0300

    Add Model project and View, ViewModel placeholders

---
+++ code/larin-anton/View/src/main/java/ru/unn/agile/personalfinance/view/PersonalFinanceApp.java
@@ -0,0 +1,18 @@
+package ru.unn.agile.personalfinance.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PersonalFinanceApp extends Application {
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws IOException {
+        // TODO
+    }
+}
---
+++ code/larin-anton/View/src/test/java/ru/unn/agile/PersonalFinance/view/FakeViewTests.java
@@ -0,0 +1,13 @@
+package ru.unn.agile.PersonalFinance.view;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class FakeViewTests {
+    @Test @Ignore
+    public void fakeTest() throws Exception {
+        fail("The fake test to prevent Travis failing (dirty hack)");
+    }
+}
