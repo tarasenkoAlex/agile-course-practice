0bb6784998e9a2321062d6adb27d30789dacbd89
Redkin Michail <maxop151@mail.ru>
2016-11-27 17:02:33 +0300

    Add forgotten files

---
+++ code/Yevtyushin-Valery/Model/src/test/MortrageDataTest.java
@@ -0,0 +1,31 @@
+package test;
+/**
+ * Created by Yevtyushin Valery on 15.11.2016.
+ */
+
+import main.mortrage.MortrageDataFactory;
+import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
+public class MortrageDataTest {
+
+    private final double debt = 7000000;
+    private final double years = 10;
+    private final double percents = 20;
+
+    @Test (expected = InvalidParameterException.class)
+    public void testThatCheckInvalidParameterDebt() {
+        MortrageDataFactory.getMortrageData(-debt, years, percents);
+    }
+
+    @Test (expected = InvalidParameterException.class)
+    public void testThatCheckInvalidParameterYears() {
+        MortrageDataFactory.getMortrageData(debt, -years, percents);
+    }
+
+    @Test (expected = InvalidParameterException.class)
+    public void testThatCheckInvalidParameterPercents() {
+        MortrageDataFactory.getMortrageData(debt, years, -percents);
+    }
+}
