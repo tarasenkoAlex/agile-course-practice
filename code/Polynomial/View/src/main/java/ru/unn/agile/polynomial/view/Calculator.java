64306ac26ebd4a4b49a41d9e5a4b24e733a32fcb
shochin.alexandr <shochin.alexandr@gmail.com>
2016-11-28 01:14:39 +0300

    Markup was created

--- code/Polynomial/View/src/main/java/ru/unn/agile/polynomial/view/Calculator.java
+++ code/Polynomial/View/src/main/java/ru/unn/agile/polynomial/view/Calculator.java
@@ -1,5 +1,24 @@
 package ru.unn.agile.polynomial.view;
 
-public class Calculator {
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
 
+public class Calculator {
+    @FXML
+    private Label secondOperandLabel;
+    @FXML
+    private TextField firstOperandTextField;
+    @FXML
+    private TextField secondOperandTextField;
+    @FXML
+    private TextField resultTextField;
+    @FXML
+    private ChoiceBox operationsChoiceBox;
+    @FXML
+    private Button calculationButton;
+    @FXML
+    private Label statusLabel;
 }
--- code/Polynomial/View/src/main/java/ru/unn/agile/polynomial/view/Main.java
+++ code/Polynomial/View/src/main/java/ru/unn/agile/polynomial/view/Main.java
@@ -14,6 +14,9 @@ public class Main extends Application {
         primaryStage.setTitle("Polynomials Calculator");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        double height = primaryStage.getHeight();
+        primaryStage.setMaxHeight(height);
+        primaryStage.setMinHeight(height);
     }
 
     public static void main(final String[] args) {
