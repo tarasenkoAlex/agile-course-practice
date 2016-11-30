346a0969b9584e149fb8b751602ed137efcb136e
Ruslan Israfilov <ruslan.is@outlook.com>
2016-11-30 23:05:36 +0300

    :lipstick: Merge two tests classes connected woth Account

---
+++ code/larin-anton/ViewModel/src/test/java/ru/unn/agile/PersonalFinance/ViewModel/WhenSavingAccount.java
@@ -0,0 +1,51 @@
+package ru.unn.agile.PersonalFinance.ViewModel;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.unn.agile.PersonalFinance.Model.Account;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static ru.unn.agile.PersonalFinance.ViewModel.AssertHelper.assertContains;
+
+public class WhenSavingAccount {
+    private LedgerViewModel ledgerViewModel;
+    private AccountViewModel accountViewModel;
+
+    @Before
+    public void setUp() throws Exception {
+        ledgerViewModel = new LedgerViewModel();
+        accountViewModel = new AccountViewModel(ledgerViewModel);
+    }
+
+    @Test
+    public void andItIsAddedToAccountList() throws Exception {
+        accountViewModel.save();
+
+        List<AccountViewModel> accountVMs = ledgerViewModel.getAccounts();
+        assertContains(accountVMs, accountViewModel);
+    }
+
+    @Test
+    public void andModelAccountNameEqualsToViewModelAccountName() throws Exception {
+        String accountName = "Cash";
+        accountViewModel.setName(accountName);
+
+        accountViewModel.save();
+
+        Account account = accountViewModel.getModelAccount();
+        assertEquals(account.getName(), accountName);
+    }
+
+    @Test
+    public void andModelAccountBalanceEqualsToViewModelAccountBalance() throws Exception {
+        int accountBalance = 10000;
+        accountViewModel.setBalance(accountBalance);
+
+        accountViewModel.save();
+
+        Account account = accountViewModel.getModelAccount();
+        assertEquals(account.getBalance(), accountBalance);
+    }
+}
