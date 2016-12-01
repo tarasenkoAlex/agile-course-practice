ffec1cb4afea23252bda9e6c9e940cc4043606d2
Anton Larin <edding.default@gmail.com>
2016-12-01 19:06:00 +0300

    Modify the format of the date picker

--- code/todo-list/View/src/main/java/ru/unn/agile/todoapp/view/TodoApp.java
+++ code/todo-list/View/src/main/java/ru/unn/agile/todoapp/view/TodoApp.java
@@ -5,8 +5,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 import ru.unn.agile.todoapp.viewmodel.TaskListCellViewModel;
 import ru.unn.agile.todoapp.viewmodel.TodoAppViewModel;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TodoApp {
     @FXML
@@ -22,6 +26,20 @@ public class TodoApp {
 
     @FXML
     private void initialize() {
+        taskDueDatePicker.setConverter(new StringConverter<LocalDate>() {
+            private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            @Override
+            public String toString(LocalDate date) {
+                return date.format(dateFormatter);
+            }
+
+            @Override
+            public LocalDate fromString(String dateString) {
+                return LocalDate.parse(dateString, dateFormatter);
+            }
+        });
+
         taskDescriptionTextField.textProperty().bindBidirectional(
                 viewModel.newTaskDescriptionProperty());
         taskDueDatePicker.valueProperty().bindBidirectional(viewModel.newTaskDueDateProperty());
