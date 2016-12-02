5292513097f7aa55d634711dbb6718294e364b04
Sirotin Nikita <niksir20@mail.ru>
2016-12-02 19:30:45 +0300

    Revert "Малойкина Елена - Лабораторная №2"

--- code/dobroliubova-evgeniia/src/main/java/ru/unn/agile/CurrencyConverter/Converter.java
+++ code/dobroliubova-evgeniia/src/main/java/ru/unn/agile/CurrencyConverter/Converter.java
@@ -1,5 +1,9 @@
-package ru.unn.agile.CurrencyConverter.model;
+package ru.unn.agile.CurrencyConverter;
 
+
+/**
+ * Created by Jane on 03.11.2016.
+ */
 public class Converter {
     public double execute(final double amount, final double fromCurrency, final double toCurrency) {
         if (amount < 0) {
