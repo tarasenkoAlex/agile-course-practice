b45a3efa00e5456ada052d3f2119a74188024c6b
Соловьев Дмитрий <qwert182@users.noreply.github.com>
2016-12-02 16:01:50 +0300

    View: extracted class "PossibilityTable"

--- code/Statistics/View/src/main/java/ru/unn/agile/Statistics/view/Calculator.java
+++ code/Statistics/View/src/main/java/ru/unn/agile/Statistics/view/Calculator.java
@@ -3,9 +3,6 @@ package ru.unn.agile.Statistics.view;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
-import java.awt.Font;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -17,13 +14,6 @@ import ru.unn.agile.Statistics.viewmodel.ViewModel;
 
 
 public class Calculator {
-    private static final char
-            RANDOM_VARIABLE_LABEL = 'x',
-            POSSIBILITY_LABEL = 'P';
-    private static final int
-            COLUMN_WIDTH = 50;
-
-
     private JPanel mainPanel;
     private JTable table;
     private JComboBox<Operation> operationComboBox;
@@ -34,103 +24,30 @@ public class Calculator {
     private JTextField deltaText;
     private JSpinner nSpinner;
 
+    private PossibilityTable possibilityTable;
+
     private final ViewModel viewModel;
-
-    private final RowNumberRenderer rowNumberRenderer;
-
-
-    private class TableBinding extends AbstractTableModel {
-        @Override
-        public int getRowCount() {
-            return 2;
-        }
-        @Override
-        public int getColumnCount() {
-            return 1 + viewModel.getValues().length;
-        }
-        @Override
-        public Object getValueAt(final int i, final int j) {
-            if (j == 0) {
-                return i == 0 ? RANDOM_VARIABLE_LABEL : POSSIBILITY_LABEL;
-            }
-            return (i == 0 ? viewModel.getValues() : viewModel.getPossibilities())[j - 1];
-        }
-        @Override
-        public boolean isCellEditable(final int i, final int j) {
-            return j != 0;
-        }
-        @Override
-        public void setValueAt(final Object aValue, final int i, final int j) {
-            double value;
-            try {
-                value = Double.valueOf((String) aValue);
-            } catch (NumberFormatException e) {
-                return;
-            }
-            (i == 0 ? viewModel.getValues() : viewModel.getPossibilities())[j - 1] = value;
-        }
-    }
-
-
-
 
 
     private void createUIComponents() {
-        table = new JTable() {
+        possibilityTable = new PossibilityTable(viewModel) {
             @Override
-            public boolean getScrollableTracksViewportWidth() {
-                return getPreferredSize().width < getParent().getWidth();
-            }
-            @Override
-            public void editingStopped(final ChangeEvent e) {
-                super.editingStopped(e);
+            public void stateChanged(final ChangeEvent e) {
                 updateStatus();
             }
         };
+
+        table = possibilityTable.getTable();
     }
 
-
-    private void updateTable() {
-        table.getColumnModel().getColumn(0).setCellRenderer(rowNumberRenderer);
-        table.getColumnModel().getColumn(0).setMaxWidth(COLUMN_WIDTH);
-
-        for (int i = 0; i < table.getColumnCount(); ++i) {
-            table.getColumnModel().getColumn(i).setMinWidth(COLUMN_WIDTH);
-        }
-        Dimension d = new Dimension(
-                COLUMN_WIDTH * table.getColumnCount(),
-                table.getPreferredSize().height
-        );
-
-        table.setPreferredSize(d);
-    }
-
-    private void updateArraysSize() {
-        viewModel.setArraysSize((int) nSpinner.getValue()); // instead of bind()
-        ((AbstractTableModel) table.getModel()).fireTableStructureChanged();
-        updateTable();
-        backBindAll();
-    }
 
     public Calculator(final ViewModel viewModel) {
         this.viewModel = viewModel;
 
-
-        rowNumberRenderer = new RowNumberRenderer(table);
-        table.setTableHeader(null);
-
-        table.setModel(new TableBinding());
-
-        updateTable();
-
-
-
         backBindAll();
-
         loadListOfOperations();
 
-
-        DocumentListener documentListener = new DocumentListener() {
+        DocumentListener updateOnTextChangedListener = new DocumentListener() {
             @Override
             public void insertUpdate(final DocumentEvent e) {
                 updateStatus();
@@ -144,8 +61,7 @@ public class Calculator {
                 updateStatus();
             }
         };
-
-        KeyAdapter calculateWhenEnterReleasedListener = new KeyAdapter() {
+        KeyAdapter calculateOnEnterReleasedListener = new KeyAdapter() {
             @Override
             public void keyReleased(final KeyEvent e) {
                 if (e.getKeyChar() == '\n') {
@@ -154,25 +70,23 @@ public class Calculator {
             }
         };
 
-
-        nSpinner.addChangeListener(e -> updateArraysSize());
-        nSpinner.addKeyListener(calculateWhenEnterReleasedListener);
+        nSpinner.addChangeListener(e -> updateTableSize());
+        nSpinner.addKeyListener(calculateOnEnterReleasedListener);
 
         deltaText.addActionListener(e -> calculate());
-        deltaText.getDocument().addDocumentListener(documentListener);
+        deltaText.getDocument().addDocumentListener(updateOnTextChangedListener);
 
-        table.addKeyListener(calculateWhenEnterReleasedListener);
+        table.addKeyListener(calculateOnEnterReleasedListener);
 
         operationComboBox.addActionListener(e -> updateAll());
-        operationComboBox.addKeyListener(calculateWhenEnterReleasedListener);
+        operationComboBox.addKeyListener(calculateOnEnterReleasedListener);
 
         momentOrderText.addActionListener(e -> calculate());
-        momentOrderText.getDocument().addDocumentListener(documentListener);
+        momentOrderText.getDocument().addDocumentListener(updateOnTextChangedListener);
 
         computeButton.addActionListener(e -> calculate());
-        //deltaText.addKeyListener(calculateWhenEnterReleasedListener);
-        //momentOrderText.addKeyListener(calculateWhenEnterReleasedListener);
     }
+
 
     public void applyTo(final JFrame frame) {
         frame.setContentPane(mainPanel);
@@ -192,6 +106,10 @@ public class Calculator {
         viewModel.setOperation((Operation) operationComboBox.getSelectedItem());
     }
 
+    private void bindArraysSize(final int newSize) {
+        viewModel.setArraysSize(newSize);
+    }
+
 
     private void backBindAll() {
         deltaText.setText(viewModel.getDelta());
@@ -204,7 +122,6 @@ public class Calculator {
 
         nSpinner.setValue(viewModel.getValues().length);
     }
-
 
     private void backBindResultAndStatus() {
         resultText.setText(viewModel.getResult());
@@ -223,29 +140,17 @@ public class Calculator {
         backBindResultAndStatus();
     }
 
+    private void updateTableSize() {
+        bindArraysSize((int) nSpinner.getValue());
+        ((AbstractTableModel) table.getModel()).fireTableStructureChanged();
+        possibilityTable.update();
+        backBindAll();
+    }
+
+
     private void calculate() {
         bind();
         viewModel.calculate();
         backBindResultAndStatus();
-    }
-
-    private final class RowNumberRenderer extends DefaultTableCellRenderer {
-        RowNumberRenderer(final JTable table) {
-            JTableHeader header = table.getTableHeader();
-            setForeground(header.getForeground());
-            setBackground(header.getBackground());
-            setFont(header.getFont());
-            setHorizontalAlignment(JLabel.CENTER);
-            setFont(getFont().deriveFont(Font.BOLD));
-            setBorder(UIManager.getBorder("TableHeader.cellBorder"));
-        }
-        @Override
-        public Component getTableCellRendererComponent(
-                final JTable table, final Object value, final boolean isSelected,
-                final boolean hasFocus, final int row, final int column
-        ) {
-            setText(value.toString());
-            return this;
-        }
     }
 }
