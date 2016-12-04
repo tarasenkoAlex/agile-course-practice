e1586d0357f4bf2ac4a230f35609d24e4b2ff664
sovrasov <sovrasov.vlad@gmail.com>
2016-12-04 13:37:18 +0300

    Add default state test for viewModel

--- code/newton-method/ViewModel/src/test/java/ru/unn/agile/NewtonRoots/viewmodel/NewtonRootAppViewModelTests.java
+++ code/newton-method/ViewModel/src/test/java/ru/unn/agile/NewtonRoots/viewmodel/NewtonRootAppViewModelTests.java
@@ -1,11 +1,34 @@
 package ru.unn.agile.NewtonRoots.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class NewtonRootAppViewModelTests  {
-    @Test
-    public void createModel() {
+import static org.junit.Assert.*;
 
+
+public class NewtonRootAppViewModelTests  {
+    private NewtonRootAppViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new NewtonRootAppViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void checkDefaultState() {
+        assertEquals("", viewModel.leftPointProperty().get());
+        assertEquals("", viewModel.rightPointProperty().get());
+        assertEquals("", viewModel.maxIterationsProperty().get());
+        assertEquals("", viewModel.accuracyProperty().get());
+        assertEquals("", viewModel.functionProperty().get());
+        assertEquals(true, viewModel.findRootButtonDisableProperty.get());
+        assertEquals("", viewModel.solverReportProperty.get());
     }
 
 }
