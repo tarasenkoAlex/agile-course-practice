ae8b7c371948e2f2e0f73b080027a7fe1c110b36
sovrasov <sovrasov.vlad@gmail.com>
2016-12-04 14:06:45 +0300

    Add bindings in model

--- code/newton-method/View/src/main/java/ru/unn/agile/NewtonRoots/view/NewtonRootsApp.java
+++ code/newton-method/View/src/main/java/ru/unn/agile/NewtonRoots/view/NewtonRootsApp.java
@@ -1,5 +1,38 @@
 package ru.unn.agile.NewtonRoots.view;
 
-public class NewtonRootsApp  {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import ru.unn.agile.NewtonRoots.viewmodel.NewtonRootAppViewModel;
 
+public class NewtonRootsApp  {
+    @FXML
+    private NewtonRootAppViewModel viewModel;
+    @FXML
+    private TextField leftPointText;
+    @FXML
+    private TextField rightPointText;
+    @FXML
+    private TextField maxIterationsText;
+    @FXML
+    private TextField accuracyText;
+    @FXML
+    private TextField functionText;
+    @FXML
+    private TextArea reportTextField;
+    @FXML
+    private Button findRootButton;
+
+    @FXML
+    private void initialize() {
+        leftPointText.textProperty().bindBidirectional(viewModel.leftPointProperty());
+        rightPointText.textProperty().bindBidirectional(viewModel.rightPointProperty());
+        maxIterationsText.textProperty().bindBidirectional(viewModel.maxIterationsProperty());
+        accuracyText.textProperty().bindBidirectional(viewModel.accuracyProperty());
+        functionText.textProperty().bindBidirectional(viewModel.accuracyProperty());
+        reportTextField.textProperty().bindBidirectional(viewModel.solverReportProperty());
+
+        findRootButton.setOnAction(value -> viewModel.findRoot());
+    }
 }
