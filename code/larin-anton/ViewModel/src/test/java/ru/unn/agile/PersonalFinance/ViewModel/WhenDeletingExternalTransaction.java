1008862f3d2a0440f3b0c291b7de2e017acbc41d
Ruslan Israfilov <ruslan.is@outlook.com>
2016-12-08 22:21:33 +0300

    Add tests for external transaction deleting

---
+++ code/larin-anton/ViewModel/src/test/java/ru/unn/agile/PersonalFinance/ViewModel/WhenDeletingExternalTransaction.java
@@ -0,0 +1,27 @@
+package ru.unn.agile.PersonalFinance.ViewModel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static ru.unn.agile.PersonalFinance.ViewModel.AssertHelper.assertNotContains;
+
+public class WhenDeletingExternalTransaction {
+    private LedgerViewModel ledger;
+    private AccountViewModel account;
+    private ExternalTransactionViewModel transaction;
+
+    @Before
+    public void setUp() throws Exception {
+        ViewModelObjectsMaker maker = new ViewModelObjectsMaker();
+        ledger = maker.getLedger();
+        account = maker.makeAccountSaved();
+        transaction = maker.makeExternalTransactionSaved(account);
+    }
+
+    @Test
+    public void andItIsDeletedFromTransactionsList() throws Exception {
+        transaction.delete();
+
+        assertNotContains(account.getTransactions(), transaction);
+    }
+}
