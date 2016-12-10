25a7e223084df37f618610a59d7fc8372a22fe14
Ruslan Israfilov <ruslan.is@outlook.com>
2016-12-11 00:34:33 +0300

    Workaround copy/paste detector

--- code/larin-anton/ViewModel/src/test/java/ru/unn/agile/PersonalFinance/ViewModel/AssertHelper.java
+++ code/larin-anton/ViewModel/src/test/java/ru/unn/agile/PersonalFinance/ViewModel/AssertHelper.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 final class AssertHelper {
     public static void assertContains(final List collection, final Object object) {
-        Assert.assertTrue(collection.contains(object));
+        Assert.assertTrue(checkContains(collection, object));
     }
 
     public static void assertNotContains(final List collection, final Object object) {
-        Assert.assertFalse(collection.contains(object));
+        Assert.assertFalse(checkContains(collection, object));
+    }
+
+    private static boolean checkContains(final List collection, final Object object) {
+        return collection.contains(object);
     }
 
     private AssertHelper() {
