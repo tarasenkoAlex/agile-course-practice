04108ac950dd5a825f05a23bdca920cfced29bc4
dima <dimadimadima051294@gmail.com>
2016-12-10 12:57:37 +0300

    fixed file

--- code/voronin-dmitry/src/main/java/ru/unn/agile/SquareCalculator/Model/SquareCalculator.java
+++ code/voronin-dmitry/src/main/java/ru/unn/agile/SquareCalculator/Model/SquareCalculator.java
@@ -17,7 +17,7 @@ public class SquareCalculator {
 
     }
 
-    public double getConeSquare(final double radius,final double rulingofcone) {
+    public double getConeSquare(final double radius, final double rulingofcone) {
         checkParams("Radius and Ruling Of Cone must not be negative!", radius , rulingofcone);
         return Math.PI * radius * (radius + rulingofcone);
     }
