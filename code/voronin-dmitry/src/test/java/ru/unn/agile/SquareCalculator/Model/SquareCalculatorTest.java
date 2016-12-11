105992d8b25baf8dd54bd02d5a5f7772dde052dc
dima <dimadimadima051294@gmail.com>
2016-12-11 05:48:19 +0300

    fixed file

--- code/voronin-dmitry/src/test/java/ru/unn/agile/SquareCalculator/Model/SquareCalculatorTest.java
+++ code/voronin-dmitry/src/test/java/ru/unn/agile/SquareCalculator/Model/SquareCalculatorTest.java
@@ -21,249 +21,249 @@ public class SquareCalculatorTest {
     }
 
     @Test
-    public void calculSquareSphereWhenRad0(){
+    public void calculSquareSphereWhenRad0() {
 
         double result = calculator.getSphereSquare(0);
 
-        assertEquals(0,result, DELTA);
+        assertEquals(0, result, DELTA);
 
     }
 
     @Test
-    public void calculSquareSphereWhenRad5Dot1(){
+    public void calculSquareSphereWhenRad5Dot1() {
 
         double result = calculator.getSphereSquare(5.1);
 
-        assertEquals(326.851299679482 , result , DELTA );
+        assertEquals(326.851299679482, result, DELTA );
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareSphereWhenRadiusNegative(){
+    public void calculSquareSphereWhenRadiusNegative() {
 
         calculator.getSphereSquare(-5.1);
 
     }
 
     @Test
-    public void calculSquareConeWhenRadius0AndRulOfCone5(){
+    public void calculSquareConeWhenRadius0AndRulOfCone5() {
 
         double result = calculator.getConeSquare(0, 5);
 
-        assertEquals(0 , result , DELTA );
+        assertEquals(0, result, DELTA );
 
     }
 
     @Test
-    public void calculSquareConeWhenRad5AndRulOfCone0(){
+    public void calculSquareConeWhenRad5AndRulOfCone0() {
 
         double result = calculator.getConeSquare(5, 0);
 
-        assertEquals(78.53981633974483 , result , DELTA );
+        assertEquals(78.53981633974483, result, DELTA );
 
     }
 
     @Test
-    public void calculSquareConeWhenRad0AndRulOfCone0(){
+    public void calculSquareConeWhenRad0AndRulOfCone0() {
 
         double result = calculator.getConeSquare(0, 0);
 
-        assertEquals(0 , result , DELTA );
+        assertEquals(0, result, DELTA );
 
     }
 
     @Test
-    public void calculSquareConeWhenRad1Dot1AndRulOfCone10Dot1(){
+    public void calculSquareConeWhenRad1Dot1AndRulOfCone10Dot1() {
 
         double result = calculator.getConeSquare(1.1 , 10.1);
 
-        assertEquals(38.70442149222625 , result , DELTA );
+        assertEquals(38.70442149222625, result, DELTA );
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareConeWhenRadNegativeAndRulOfCone5(){
+    public void calculSquareConeWhenRadNegativeAndRulOfCone5() {
 
-        calculator.getConeSquare(-1 , 5);
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void calculSquareConeWhenRad5AndRulOfConeNegative(){
-
-        calculator.getConeSquare(5 , -1);
+        calculator.getConeSquare(-1, 5);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareConeWhenRaAndRulOfConeNegative(){
+    public void calculSquareConeWhenRad5AndRulOfConeNegative() {
 
-        calculator.getConeSquare(-1 , -1);
+        calculator.getConeSquare(5, -1);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calculSquareConeWhenRaAndRulOfConeNegative() {
+
+        calculator.getConeSquare(-1, -1);
 
     }
 
     @Test
-    public void calculSquareCubeWhenLenght0(){
+    public void calculSquareCubeWhenLenght0() {
 
         double result = calculator.getCubeSquare(0);
 
-        assertEquals(0 , result , DELTA );
+        assertEquals(0, result, DELTA );
 
     }
 
     @Test
-    public void calculSquareCubeWhenLenght5(){
+    public void calculSquareCubeWhenLenght5() {
 
         double result = calculator.getCubeSquare(5);
 
-        assertEquals(150 , result , DELTA );
+        assertEquals(150, result, DELTA);
 
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public  void calculSquareCubeWhenLenghtNegative(){
+    public  void calculSquareCubeWhenLenghtNegative() {
 
         calculator.getCubeSquare(-5);
 
     }
 
     @Test
-    public void calculSquareCylinderWhenRad0AndHeght0(){
+    public void calculSquareCylinderWhenRad0AndHeght0() {
 
         double result = calculator.getCylinderSquare(0, 0);
 
-        assertEquals(0, result , DELTA );
+        assertEquals(0, result, DELTA);
 
     }
 
     @Test
-    public void calculSquareCylinderWhenRad0AndHeight5(){
+    public void calculSquareCylinderWhenRad0AndHeight5() {
 
         double result = calculator.getCylinderSquare(0, 5);
 
-        assertEquals(0 , result , DELTA );
+        assertEquals(0, result, DELTA);
 
     }
 
     @Test
-    public  void calculSquareCylinderWhenRad5AndHeight0(){
+    public  void calculSquareCylinderWhenRad5AndHeight0() {
 
         double result = calculator.getCylinderSquare(5, 0);
 
-        assertEquals(157.07963267948966 , result , DELTA );
+        assertEquals(157.07963267948966, result, DELTA);
 
     }
 
     @Test
-    public  void calculSquareCylinderWhenRad5AndHeight5(){
+    public  void calculSquareCylinderWhenRad5AndHeight5() {
 
         double result = calculator.getCylinderSquare(5, 5);
 
-        assertEquals(314.1592653589793 , result , DELTA );
+        assertEquals(314.1592653589793, result, DELTA);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public  void calculSquareCylinderWhenRadNegativeAndHeight1(){
+    public  void calculSquareCylinderWhenRadNegativeAndHeight1() {
 
         calculator.getCylinderSquare(-1, 1);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public  void calculSquareCylinderWhenRad1AndHeightNegative(){
+    public  void calculSquareCylinderWhenRad1AndHeightNegative() {
 
         calculator.getCylinderSquare(1, -1);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public  void calculSquareCylinderWhenRadAndHeightNegative(){
+    public  void calculSquareCylinderWhenRadAndHeightNegative() {
 
         calculator.getCylinderSquare(-1, -1);
 
     }
 
     @Test
-    public void calculSquareParallelepipedWhenHeight0Lenght0Width0(){
+    public void calculSquareParallelepipedWhenHeight0Lenght0Width0() {
 
         double result = calculator.getParallelSquare(0, 0, 0);
 
-        assertEquals(0, result , DELTA );
+        assertEquals(0, result, DELTA);
 
     }
 
     @Test
-    public void calculSquareParallelepipedWhenHeight0Lenght2Width2(){
+    public void calculSquareParallelepipedWhenHeight0Lenght2Width2() {
 
         double result = calculator.getParallelSquare(0, 2, 2);
 
-        assertEquals(8, result , DELTA );
+        assertEquals(8, result, DELTA);
 
     }
 
     @Test
-    public void calculSquareParallelepipedWhenHeight2Lenght0Width2(){
+    public void calculSquareParallelepipedWhenHeight2Lenght0Width2() {
 
         double result = calculator.getParallelSquare(2, 0, 2);
 
-        assertEquals(8, result , DELTA );
+        assertEquals(8, result, DELTA);
 
     }
 
     @Test
-    public void calculSquareParallelepipedWhenHeight2Lenght2Width0(){
+    public void calculSquareParallelepipedWhenHeight2Lenght2Width0() {
 
         double result = calculator.getParallelSquare(2, 2, 0);
 
-        assertEquals(8, result , DELTA );
+        assertEquals(8, result, DELTA);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeightNegativeLenght2Width2(){
+    public void calculSquareParallelepipedWhenHeightNegativeLenght2Width2() {
 
         calculator.getParallelSquare(-2, 2, 2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeight2eLenghtNegativeWidth2(){
+    public void calculSquareParallelepipedWhenHeight2eLenghtNegativeWidth2() {
 
         calculator.getParallelSquare(2, -2, 2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeight2Lenght2WidthNegative(){
+    public void calculSquareParallelepipedWhenHeight2Lenght2WidthNegative() {
 
         calculator.getParallelSquare(2, 2, -2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeighAndLenghtNegativeWidth2(){
+    public void calculSquareParallelepipedWhenHeighAndLenghtNegativeWidth2() {
 
         calculator.getParallelSquare(-2, -2, 2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeight2LenghtAndWidthNegative(){
+    public void calculSquareParallelepipedWhenHeight2LenghtAndWidthNegative() {
 
         calculator.getParallelSquare(2, -2, -2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeightAndWidthNegativeLenght2(){
+    public void calculSquareParallelepipedWhenHeightAndWidthNegativeLenght2() {
 
         calculator.getParallelSquare(-2, 2, -2);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void calculSquareParallelepipedWhenHeightLenghtWidthNegative(){
+    public void calculSquareParallelepipedWhenHeightLenghtWidthNegative() {
 
         calculator.getParallelSquare(-2, -2, -2);
 
